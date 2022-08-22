20632df8e6c353c21e2ab31e9d2af266706e0513
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-22 20:19:52 -0300

    Inserindo escopo do projeto

---
+++ src/main/java/com/example/listaDeProdutosValidacoes/ListaDeProdutosValidacoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.listaDeProdutosValidacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeProdutosValidacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeProdutosValidacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/listaDeProdutosValidacoes/ListaDeProdutosValidacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listaDeProdutosValidacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeProdutosValidacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
